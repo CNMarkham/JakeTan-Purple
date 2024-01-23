cc80e4d0ab64f25f94c95a7a3b3e58ba361c3e70
Roosham <markhamonca@codeninjas.com>
2024-01-23 16:53:36 -0500

    saved?
    save

--- JakeTan - PongV2/Assets/Player2script.cs
+++ JakeTan - PongV2/Assets/Player2script.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Script : MonoBehaviour
+public class Player2script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 tempPosition;   
     }
 }
