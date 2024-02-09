9704f2a6fedfd416743beaf84410e0a218108261
BottymcBotFace <markhamonca@codeninjas.com>
2024-02-08 19:50:18 -0500

    step 19
    ee

--- Breakout/Assets/GameManager.cs
+++ Breakout/Assets/GameManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject brickPrefab;
+    public float xSpace;
+    public float xOffset;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        for (int x = 0; x < 10; x++)
+        {
+            Instantiate(brickPrefab, new Vector2(x * xSpace+ xOffset, 3), Quaternion.identity);
+        }
     }
 }
