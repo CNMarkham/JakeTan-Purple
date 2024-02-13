10330136a1350287071f7c0b3c9d5a33775a4aba
I AM jt <markhamonca@codeninjas.com>
2024-02-13 16:38:31 -0500

    Done
    Done(Breakout)

--- Breakout/Assets/GameManager.cs
+++ Breakout/Assets/GameManager.cs
@@ -12,6 +12,9 @@ public class GameManager : MonoBehaviour
         for (int x = 0; x < 10; x++)
         {
             Instantiate(brickPrefab, new Vector2(x * xSpace+ xOffset, 3), Quaternion.identity);
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 3.75f), Quaternion.identity);
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 4.5f), Quaternion.identity);
+
         }
     }
 }
--- Breakout/Assets/PlayerMovement.cs
+++ Breakout/Assets/PlayerMovement.cs
@@ -10,14 +10,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        /*if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector2.left * Time.deltaTime * speed);
         }
         if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector2.right * Time.deltaTime * speed);
-        }
-
+        }*/
+        float y = transform.position.y;
+        float x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        Vector2 tempPosition = new Vector2(x, y);
+        transform.position = tempPosition;
     }
 }
