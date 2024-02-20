a8dcddb4d43c4cb53a9cbd37da223af7685b9c65
Madelyn <markhamonca@codeninjas.com>
2024-02-20 16:50:39 -0500

    Space
    step 31

---
+++ Space Invaders/Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    public int speed;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+    }
+}
