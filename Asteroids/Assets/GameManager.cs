8d93734e0bd15b5fed3ae5583634aa09b17a969a
Chelsea <markhamonca@codeninjas.com>
2024-03-14 20:01:07 -0400

    Step 70 of asteroids
    Done cloning the asteroids script and the minSize, need to do step 71

--- Asteroids/Assets/GameManager.cs
+++ Asteroids/Assets/GameManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject rockPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-8, 8), Random.Range(-8, 8), 0f);
+            Instantiate(rockPrefab, randomPosition, Quaternion.identity);
+        }
+
     }
 
     // Update is called once per frame
@@ -16,3 +22,4 @@ public class GameManager : MonoBehaviour
         
     }
 }
+ 
