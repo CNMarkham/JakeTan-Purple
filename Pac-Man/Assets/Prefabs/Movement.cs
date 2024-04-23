d9307a35973da3e2b639cf46c4b06d8219080f31
Roosham <markhamonca@codeninjas.com>
2024-04-23 16:51:08 -0400

    JAKE step 54
    d

--- Pac-Man/Assets/Prefabs/Movement.cs
+++ Pac-Man/Assets/Prefabs/Movement.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public abstract class Movement : MonoBehaviour
 {
-    public float speed; 
-    public Vector2 initialDirection;
-    public LayerMask obstacleLayer;
+    public float speed; // speed = 5
+    public Vector2 initialDirection; // x = 1, y = 0 [1,0]
+    public LayerMask obstacleLayer; // Pacman
 
-    protected Rigidbody2D rb;
-    protected Vector2 direction;
-    protected Vector2 nextDirection;
+    protected Rigidbody2D rb; // GetComponent
+    protected Vector2 direction; // x, y
+    protected Vector2 nextDirection; // x, y
     // Start is called before the first frame update
     void Start()
     {
@@ -27,16 +27,16 @@ public abstract class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 position = rb.position;
-        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+        Vector2 position = rb.position;// getting a reference to the rigidbody
+        Vector2 translation = direction * speed * Time.fixedDeltaTime; // the actual movement for the gameobject
 
-        rb.MovePosition(position + translation);
+        rb.MovePosition(position + translation); // this code does the actual movement in the game
     }
-private bool Occupied(Vector2 newDirection)
-{
-        //RaycastHit2D hit = Phsyics2D.BoxCast();
-        return true;
-}
+    private bool Occupied(Vector2 newDirection)// Checking if we want to go is occupied( blocked)English translation.
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1.5f, obstacleLayer);//raycast shoots an invisible line sensing if anything is there if true, player can continue moving
+        return hit.collider != null; 
+    } 
 }
   
 
