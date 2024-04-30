ab718578d18df8fe6821362108c8d21732f24c26
Roosham <markhamonca@codeninjas.com>
2024-04-30 16:51:22 -0400

    Step 74

--- Pac-Man/Assets/Prefabs/Pacman.cs
+++ Pac-Man/Assets/Prefabs/Pacman.cs
@@ -6,6 +6,13 @@ public class Pacman : Movement
 {
     protected override void ChildUpdate()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            SetDirection(new Vector2(horizontal, vertical));
+        }
+        transform.right = direction; 
     }
 }
