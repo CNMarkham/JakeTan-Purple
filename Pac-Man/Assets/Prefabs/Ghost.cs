16e29e994e62141c98dea1beddede87b9f096f0e
0 <132835392+CNMarkham@users.noreply.github.com>
2024-05-16 16:49:28 -0400

    Step 105
    PacMan

---
+++ Pac-Man/Assets/Prefabs/Ghost.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : Movement
+{
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+
+
+    protected override void ChildUpdate()
+    {
+
+    }
+}
