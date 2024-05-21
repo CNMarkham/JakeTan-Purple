9c4367002d5b7213f156ab510573dce4994dfefe
0 <132835392+CNMarkham@users.noreply.github.com>
2024-05-21 16:51:21 -0400

    step 107
    dd

--- Pac-Man/Assets/Prefabs/Ghost.cs
+++ Pac-Man/Assets/Prefabs/Ghost.cs
@@ -7,10 +7,42 @@ public class Ghost : Movement
     public GameObject body;
     public GameObject eyes;
     public GameObject blue;
+    public GameObject white;
+    public bool atHome;
+    public float homeDuration;
+    private bool frightened;
 
+    private void Awake()
+    {
+        
+    }
 
     protected override void ChildUpdate()
     {
 
+    }
+    private  void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D Collider)
+    {
+        Node node = collision.GetComponent<Node>();
+    }
+    private void LeaveHome()
+    {
+
+    }
+    public void Frighten()
+    {
+
+    }
+    private void Flash()
+    {
+
+    }
+    private void Reset()
+    {
+       
     }
 }
