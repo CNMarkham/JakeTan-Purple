044dc039693bba3ddcbfed9fbbbf70ca6b7d7043
Is <markhamonca@codeninjas.com>
2024-09-14 10:53:06 -0400

    Step 129
    Mario

--- Super Mario Bros/Assets/Script/Koopa.cs
+++ Super Mario Bros/Assets/Script/Koopa.cs
@@ -26,7 +26,30 @@ public class Koopa : MonoBehaviour
         {
             return;
         }
-
+        if (collision.transform.position.y > transform.position.y + 0.4f)//Checks if Mario is above Koopa sprites
+        {
+            if (shelled)//checking if above and not moving.
+            {
+                Launch();
+            }
+            else
+            {
+                GetComponent<Animator>().SetTrigger("shell");
+                GetComponent<EnemyMovement>().speed = 0;
+                GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+                shelled = true;//Just setting Koopas shell variable to true.
+            }
+            Rigidbody2D playerRB = collision.gameObject.GetComponent<Rigidbody2D>();
+            playerRB.velocity = new Vector2(playerRB.velocity.x, 10);
+        }
+        else if (shelled && !shellMoving)//When Koopa is not moving and is in his shell(walking)
+        {
+            Launch();//Launch function under this if statement.(Calling the Launch function).
+        }
+        else // Koopa is moving(this calls the hit function from PlayerBehaviior).
+        {
+            collision.gameObject.GetComponent<PlayerBehaviour>().Hit();
+        }
     }
 
     private void Launch()
