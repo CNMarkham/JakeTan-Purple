125fee0c4bf592523a9947153a8fd7bc28086c82
Is <markhamonca@codeninjas.com>
2024-10-04 17:49:37 -0400

    Step 11(c)

--- Tetris/Assets/Scenes/Tetromino.cs
+++ Tetris/Assets/Scenes/Tetromino.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tetromino : MonoBehaviour
-{ 
+{
+    private float previousTime;
+    public float fallTime = 0.8f;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3 (1, 0);
+            transform.position += new Vector3 (-1, 0);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(-1, 0);
+            transform.position += new Vector3(1, 0);
+        }
+        float tempTime = fallTime;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tempTime = tempTime / 10;
+        }
+
+
+            if (Time.time-previousTime > tempTime)
+        {
+            transform.position += Vector3.down;    
+            previousTime = Time.time;
         }
     }
+
 }
 
