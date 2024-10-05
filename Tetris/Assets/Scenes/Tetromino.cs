7bc78cd2457e7ac2b570fea837b46dd11f1d6f3a
Monica <markhamonca@codeninjas.com>
2024-10-05 10:47:57 -0400

    Step 19 or 18
    Tetris

--- Tetris/Assets/Scenes/Tetromino.cs
+++ Tetris/Assets/Scenes/Tetromino.cs
@@ -6,11 +6,14 @@ public class Tetromino : MonoBehaviour
 {
     private float previousTime;
     public float fallTime = 0.8f;
+    public static int width = 10;
+    public static int height = 20;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3 (-1, 0);
+            transform.position += new Vector3(-1, 0);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -24,12 +27,26 @@ public class Tetromino : MonoBehaviour
         }
 
 
-            if (Time.time-previousTime > tempTime)
+        if (Time.time - previousTime > tempTime)
         {
-            transform.position += Vector3.down;    
+            transform.position += Vector3.down;
             previousTime = Time.time;
         }
+
     }
 
-}
+    public bool validMove()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+            if (x <= 0 || y <= 0 || x >= width || y >= height)
+            {
 
+            }
+            
+        }
+        return false;
+    }
+}
