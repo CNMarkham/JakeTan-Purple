865b9295e22db0e1634e3b920d60b314d995456e
Monica <markhamonca@codeninjas.com>
2024-10-29 19:53:24 -0400

    Last step need to ask what code means I don't know what it means)
    Done just need to clarify next session on the bugs

--- Tetris/Assets/Scenes/Scripts/Tetromino.cs
+++ Tetris/Assets/Scenes/Scripts/Tetromino.cs
@@ -9,6 +9,7 @@ public class Tetromino : MonoBehaviour
     public static int width = 10;
     public static int height = 20;
     public Vector3 rotationPoint;
+    public static Transform[,] grid = new Transform[width, height]; 
 
     void Update()
     {
@@ -44,6 +45,8 @@ public class Tetromino : MonoBehaviour
             {
                 transform.position += new Vector3(0, 1);
                 this.enabled = false;
+                AddToGrid();
+                CheckLines();
                 FindObjectOfType<Spawner>().SpawnTetromino();
             }
         }
@@ -57,7 +60,6 @@ public class Tetromino : MonoBehaviour
             }
         }
 
-
     }
 
     public bool validMove()
@@ -70,6 +72,10 @@ public class Tetromino : MonoBehaviour
             {
                 return false;
             }
+            if (grid[x, y] != null)
+            {
+                return false;
+            }
         }
 
         return true;
@@ -79,5 +85,61 @@ public class Tetromino : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(transform.position + rotationPoint, 0.1f);
+    }
+
+    public void AddToGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+            grid[x, y] = child;
+            
+        }
+    }
+    public void CheckLines()
+    {
+        for (int i = height - 1; i >= 0; i--)
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+    }
+
+    public bool HasLine(int i)
+    {
+        for(int j=0; j < width; j++)
+        {
+            if (grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+}
+    public void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, y] != null)
+                {
+                    grid[x, y - 1] = grid[x, y];
+                    grid[x, y] = null;
+                    grid[x, y - 1].transform.position += Vector3.down;
+                }
+            }
+        }
     }
 }
